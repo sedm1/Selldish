c7837f23777b15bc2b5fbaf89b7bc70a8dfecab3
Николай <novikovn383@gmail.com>
2023-04-27 17:48:14 +0500

    ff

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 
 const routes = [
   {
@@ -62,7 +62,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  base: process.env.BASE_URL,
   routes
 })
 
