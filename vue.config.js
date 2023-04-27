94008f51260aa94845cea395797542006d1afabf
Николай <novikovn383@gmail.com>
2023-04-27 19:53:51 +0500

    loc

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,29 @@
 module.exports = {
+
   publicPath: process.env.PUBLIC_URL || "",
+
+  devServer: {
+
+    historyApiFallback: true
+
+  },
+
   css: {
+
     loaderOptions: {
+
       sass: {
+
         additionalData: `
+
           @import "@/assets/sass/_vars.sass"
+
         `
+
       }
+
     }
+
   }
+
 }
