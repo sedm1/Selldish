636e4d9bd02558ef7132f147fd93b101ffbdb5ae
Николай <novikovn383@gmail.com>
2023-04-27 18:16:51 +0500

    ggg

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,6 @@ const routes = [
 ]
 
 const router = createRouter({
-  mode: 'hash',
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.PUBLIC_URL || "",
   css: {
     loaderOptions: {
       sass: {
